1ba65c7f03f535bc733f81f8f19608a80340cf9b
Cassio Izumi <izumicassio@gmail.com>
2022-10-04 13:36:49 -0300

    section work expiriences refactored. Added favicon.

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+let slides = document.getElementsByClassName('mySlides');
+let dots = document.getElementsByClassName('dot');
+
+showSlides(0);
+
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
+}
+
+function activateSlide(n) {
+  slides[n].style.display = 'block';
+  dots[n].className += ' active';
 }
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(' active', '');
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
+  activateSlide(slideIndex - 1);
+  scrollToTitle();
+}
+
+function scrollToTitle() {
+  document.querySelector('.job-info-container').scrollIntoView({
+    behavior: 'smooth',
+  });
+}
